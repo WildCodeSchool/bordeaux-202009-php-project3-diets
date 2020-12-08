b0995a059837f79ce1499072333b3538b0607b66
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-12-08 17:04:09 +0100

    Connaissances in progress

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,31 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    /**
+    * @Route("/ressource", name="ressource")
+    */
+    public function ressource(): Response
+    {
+
+        return $this->render('ressource/index.html.twig', [
+        //    'form' => $form->createView(),
+        ]);
+    }
+/*           $form = $this->createForm(eType::class, $episode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($episode);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('episode_index');
+        }
+
+        return $this->render('episode/new.html.twig', [
+            'episode' => $episode,
+
+        ]);
+    }*/
 }
---
+++ src/Entity/RessourceType.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RessourceTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RessourceTypeRepository::class)
+ */
+class RessourceType
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $format;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $icon;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $identifier;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFormat(): ?string
+    {
+        return $this->format;
+    }
+
+    public function setFormat(string $format): self
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(string $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    public function setIdentifier(string $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+}
