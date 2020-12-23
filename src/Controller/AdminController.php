b0e8c02961bfd4753d1a952cf50d9341ebf5c2f8
Sebastien kusz <kusz.sebastien@gmail.com>
2020-12-23 21:19:15 +0100

    List registered users & count in Admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +16,28 @@ class AdminController extends AbstractController
      */
     public function index(UserRepository $userRepository): Response
     {
-        $registeredUser = $userRepository->findAll();
+        $registeredUser = $userRepository->findBy(
+            [
+            ],
+            [ 'lastname' => 'ASC'
+            ]
+        );
         return $this->render('admin/index.html.twig', [
             'registered_user_count' => count($registeredUser),
             'registered_user' => $registeredUser,
         ]);
+    }
+
+    /**
+     * @Route("/admin/{id}", name="admin_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, User $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+        return $this->redirectToRoute('admin');
     }
 }
