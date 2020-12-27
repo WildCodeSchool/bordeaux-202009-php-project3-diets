8bc93d61960f6ebd558ca6cf8f33da5447a8c475
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-12-27 18:54:41 +0100

    cleaning ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Service;
+use App\Form\ServiceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -38,7 +41,7 @@ class ProfileController extends AbstractController
      * @Route("/edit/{id}", methods={"GET"}, name="edit")
      * @return Response
      */
-    public function edit(User $user): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, User $user): Response
     {
         if (!$user) {
             throw $this->createNotFoundException(
@@ -49,7 +52,20 @@ class ProfileController extends AbstractController
                 ->getRepository(User::class)
                 ->findBy(['id' => $user->getId()]);
         }
+        $service = new Service();
+        $formService = $this->createForm(ServiceType::class, $service);
+        $formService->handleRequest($request);
+        if ($formService->isSubmitted() && $formService->isValid()) {
+            $entityManager->persist($service);
+            $entityManager->flush();
+        }
+        /*$service = $this->getDoctrine()
+            ->getRepository(Service::class)
+            ->findBy('user' => 'id');*/
+
         return $this->render('profile/edit.html.twig', [
+            'formService' => $formService->createView(),
+            'services' => $service,
             'user_infos' => $userInfos,
         ]);
     }
