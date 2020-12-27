efd3f8f6cc745eb186b19b6d6a0e5652a55b1a1a
Benjamin <63590662+FromBenj@users.noreply.github.com>
2020-12-27 18:40:56 +0100

    Profile + service

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Service;
+use App\Form\ServiceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/show/{id}", methods={"GET"}, name="show")
+     * @Route("/show/{id}", methods={"GET"}, name="show", requirements={"id":"\d+"})
      * @return Response
      */
     public function show(User $user): Response
@@ -29,6 +32,7 @@ class ProfileController extends AbstractController
                 ->getRepository(User::class)
                 ->findBy(['id' => $user->getId()]);
         }
+        //dd($user);
         return $this->render('profile/show.html.twig', [
             'user_infos' => $userInfos,
         ]);
@@ -38,7 +42,7 @@ class ProfileController extends AbstractController
      * @Route("/edit/{id}", methods={"GET"}, name="edit")
      * @return Response
      */
-    public function edit(User $user): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, User $user): Response
     {
         if (!$user) {
             throw $this->createNotFoundException(
@@ -49,7 +53,20 @@ class ProfileController extends AbstractController
                 ->getRepository(User::class)
                 ->findBy(['id' => $user->getId()]);
         }
+        $service = new Service();
+        $formService = $this->createForm(ServiceType::class, $service);
+        $formService->handleRequest($request);
+        if ($formService->isSubmitted() && $formService->isValid()) {
+            $entityManager->persist($service);
+            $entityManager->flush();
+        }
+        /*$service = $this->getDoctrine()
+            ->getRepository(Service::class)
+            ->findBy('user' => 'id');*/
+
         return $this->render('profile/edit.html.twig', [
+            'formService' => $formService->createView(),
+            'services' => $service,
             'user_infos' => $userInfos,
         ]);
     }
