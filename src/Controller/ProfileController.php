2286583872199e34ee73b22c9c069628f1736df6
Benjamin Touboul <touboul.benjamin@gmail.com>
2020-12-27 14:50:02 +0100

    Profile information

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 /**
  * @Route("/profile", name="profile_")
@@ -13,13 +16,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/show/{id}", methods={"GET"}, name="show")
+     * @Route("/show/{id}", methods={"GET"}, name="show", requirements={"id":"\d+"})
      * @return Response
      */
     public function show(User $user): Response
     {
         if (!$user) {
-            return $this->redirectToRoute('ressource');
+            throw $this->createNotFoundException(
+                'No profile with id : ' . $user->getId() . ' found in user\'s table.'
+            );
         } else {
             $userInfos = $this->getDoctrine()
                 ->getRepository(User::class)
@@ -32,12 +37,23 @@ class ProfileController extends AbstractController
     }
 
     /**
-     * @Route("/edit", name="edit")
+     * @Route("/edit/{id}", methods={"GET"}, name="edit")
+     * @return Response
      */
-    public function edit(): Response
+    public function edit(User $user): Response
     {
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No profile with id : ' . $user->getId() . ' found in user\'s table.'
+            );
+        } else {
+            $userInfos = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findBy(['id' => $user->getId()]);
+        }
+        //dd($user);
         return $this->render('profile/edit.html.twig', [
-            '' => '',
+            'user_infos' => $userInfos,
         ]);
     }
 }
