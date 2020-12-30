bdf4b0d6cf9a3e011f428ac62d3a8eeff5ddde66
gounou <edwige.gounou@gmail.com>
2020-12-30 11:32:04 +0100

    fixtures fin

---
+++ src/DataFixtures/EventFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Event;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class EventFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 50; $i++) {
+            $event = new Event();
+            $event->setName($faker->name);
+            $event->setDescription($faker->text);
+            $event->setCreatedAt($faker->dateTime);
+            $event->setDateEnd($faker->dateTime);
+            $event->setDateStart($faker->dateTime);
+            $event->setLink($faker->url);
+            $event->getPicture($this->getReference('picture_' . rand(1, 49)));
+            $event->setEventIsValidated(rand(0, 1));
+            $event->setPrice($faker->numberBetween(0, 1000));
+            $event->setUpdatedAt($faker->dateTime);
+            $event->setEventFormat($this->getReference('event_format_' . rand(1, 49)));
+            $event->setRegisteredEvent($this->getReference('registered_event_' . rand(1, 49)));
+            $manager->persist($event);
+            $this->addReference('event_' . $i, $event);
+        }
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [PictureFixtures::class];
+    }
+
+}
---
+++ src/DataFixtures/ResourceFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Resource;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class ResourceFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for($i = 1; $i < 50; $i++) {
+            $resource = new Resource();
+            $resource->setUpdatedAt($faker->dateTimeAD);
+            $resource->setLink($faker->url);
+            $resource->setCreatedAt($faker->dateTimeAD);
+            $resource->setDescription($faker->text);
+            $resource->setName($faker->word);
+            $resource->setResourceFormat($this->getReference('resource_format_' . rand(1, 49)));
+            $resource->setUser($this->getReference('user_' . rand(1, 50)));
+            $manager->persist($resource);
+            $this->addReference('resource_' . $i, $resource);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return array(ResourceFormatFixtures::class,
+        UserFixtures::class,);
+    }
+}
