a2e616e474c4cd8c78dc00c3a4d7cb1a074a1d8c
Sebastien kusz <kusz.sebastien@gmail.com>
2021-01-12 09:45:19 +0100

    Checkout branch knowledge

--- src/Controller/KnowledgeController.php
+++ src/Controller/KnowledgeController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ResourceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/knowledge", name="knowledge")
+ */
 class KnowledgeController extends AbstractController
 {
     /**
-     * @Route("/knowledge", name="knowledge")
+     * @Route("/", name="_index")
      */
-    public function index(): Response
+    public function index(ResourceRepository $resourceRepository): Response
     {
+        $resource = $resourceRepository->findBy([], ['updatedAt'=> 'DESC'], 10 );
+        /*$service = $this->getDoctrine()
+            ->getRepository(Service::class)
+            ->findBy(array(), array('id' => 'desc'));*/
+
         return $this->render('knowledge/index.html.twig', [
-            'controller_name' => 'KnowledgeController',
+            'resources' => $resource,
         ]);
     }
 }
