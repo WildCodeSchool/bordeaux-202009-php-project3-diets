8350a994cbb4e8f642051b051b2d102af3bc08d4
Benjamin Touboul <touboul.benjamin@gmail.com>
2021-01-26 11:02:19 +0100

    Knowledge page

--- src/Controller/KnowledgeController.php
+++ src/Controller/KnowledgeController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Resource;
 use App\Entity\User;
+use App\Form\ResourcesAllType;
 use App\Form\ResourceType;
+use App\Form\SearchResourceType;
 use App\Repository\ResourceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,42 +14,78 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/knowledge", name="knowledge")
- */
+
 class KnowledgeController extends AbstractController
 {
-    private const LIMIT = 10;
+    /*private const LIMIT = 10;*/
+    private const NBRESOURCE = 12;
 
     /**
-     * @Route("/{length}", name="_index")
+     * @Route("/knowledge", name="knowledge")
      */
-    public function index(
-        string $length,
-        Request $request,
-        EntityManagerInterface $entityManager,
-        ResourceRepository $resourceRepository
-    ): Response {
-        if ($length === 'last') {
-            $length = true ;
-            $resource = $resourceRepository->findBy([], ['updatedAt' => 'DESC']);
+    public function index(Request $request, ResourceRepository $resourceRepository): Response
+    {
+        $formSearch = $this->createForm(SearchResourceType::class);
+        $formSearch->handleRequest($request);
+
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) {
+            $formSearch->getData()['search'] !== null ?
+                $search = $formSearch->getData()['search'] : $search = '';
+            $formSearch->getData()['pathology'] !== null ?
+                $pathology = $formSearch->getData()['pathology']->getIdentifier() : $pathology = '';
+            $formSearch->getData()['format'] !== null ?
+                $format = $formSearch->getData()['format']->getIdentifier() : $format = '';
+            if (!$search && !$pathology && !$format) {
+                $resourcesSearch = ['last'];
+            } elseif (!$search && !$pathology && $format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchByFormat($format);
+            } elseif (!$search && $pathology && !$format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchByPathology($pathology);
+            } elseif ($search && !$pathology && !$format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchLikeName($search);
+            } elseif ($search && $pathology && !$format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchByPathologyAndLikeName($pathology, $search);
+            } elseif ($search && !$pathology && $format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchByFormatAndLikeName($format, $search);
+            } elseif (!$search && $pathology && $format) {
+                $resourcesSearch = $resourceRepository
+                    ->searchByPathologyAndFormat($pathology, $format);
+            } else {
+                $resourcesSearch = $resourceRepository
+                    ->searchByPathologyFormatAndLikeName($pathology, $format, $search);
+            }
         } else {
-            $length = false ;
-            $resource = $resourceRepository->findBy([], ['updatedAt' => 'DESC'], self::LIMIT);
-        }
-        $newResource = new Resource();
-        $formResource = $this->createForm(ResourceType::class, $newResource);
-        $formResource->handleRequest($request);
-        if ($formResource->isSubmitted() && $formResource->isValid()) {
-            $newResource->setUser($this->getUser());
-            $entityManager->persist($newResource);
-            $entityManager->flush();
+            $resourcesSearch = ['last'];
         }
 
+        $formSearchAll = $this->createForm(ResourcesAllType::class);
+        $formSearchAll->handleRequest($request);
+
+        if ($formSearchAll->isSubmitted() && $formSearchAll->isValid()) {
+            $resourcesSearch = $resourceRepository
+                ->findAll();
+        }
+
+        $resourcesLastUpdate = $resourceRepository->findBy(
+            [
+            ],
+            [
+                'updatedAt' => 'DESC'
+            ],
+            self::NBRESOURCE
+        );
+
         return $this->render('knowledge/index.html.twig', [
-            'resources' => $resource,
-            'length'    => $length,
-            'formResource' => $formResource->createView(),
+        'resourcesLastUpdate' => $resourcesLastUpdate,
+            'resourcesSearch' => $resourcesSearch,
+            'last' => ['last'],
+            'formSearch' => $formSearch->createView(),
+            'form_search_all' => $formSearchAll->createView(),
         ]);
     }
 }
