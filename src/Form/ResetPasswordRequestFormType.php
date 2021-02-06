816a7da7ea49f69728efabd814c6e2587fe5d068
Sebastien kusz <kusz.sebastien@gmail.com>
2021-02-06 18:00:05 +0100

    CSS landing page responsive

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -14,6 +14,7 @@ class ResetPasswordRequestFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
+                'label' => false,
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter your email',
